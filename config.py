274b4f2c177af281bb1a263f61767055c2964cd4
AmouRasoul <80779247+RasoulNarghani@users.noreply.github.com>
2026-05-10 17:41:07 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+BOT_TOKEN = "8657355671:AAHygoZiqu3sjnXrPDFXalPcs9z0xzvZ-Vo"
+ADMIN_ID = 1253158350
+BOT_USERNAME = "PersiaV2Ray_Bot"
+TON_WALLET = "UQDGr0gCn3cc76U5DeF_XBpxgMagKzztzz6JApoxFVOrg9bY"
+USDT_TON_WALLET = "UQDGr0gCn3cc76U5DeF_XBpxgMagKzztzz6JApoxFVOrg9bY"
+USDT_TRC20_WALLET = "TMa9Yj48Ec1wFmWk1hE5UaEZaifn6Z1p7z"
+WELCOME_TEXT = """
+سلام، خوش اومدی به ربات خودت😘🎉
+
+برای مشاهده محصولات از دکمه‌های زیر استفاده کن👇
+"""
+PRODUCTS = {
+  "1GB": {
+    "title": "1 گیگابایت",
+    "price": 1.2
+  },
+  "2GB": {
+    "title": "2 گیگابایت",
+    "price": 2.4
+  },
+  "5GB": {
+    "title": "5 گیگابایت",
+    "price": 5
+  },
+  "10GB": {
+    "title": "10 گیگابایت",
+    "price": 10
+  },
+  "20GB": {
+    "title": "20 گیگابایت",
+    "price": 15
+  }
+}
